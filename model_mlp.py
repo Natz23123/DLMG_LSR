ce27df25c57733903ddd09159e3defd0a11433c5
Peafowll <tudorpaun16@gmail.com>
2026-01-07 01:43:47 +0200

    mlp update

--- model_mlp.py
+++ model_mlp.py
@@ -5,11 +5,22 @@ class LandmarkClassifier(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(65, 130),
+            nn.Linear(65, 128),
             nn.ReLU(),
-            nn.Linear(130, 65),
+            nn.Dropout(0.3),
+            
+            nn.Linear(128, 256),
             nn.ReLU(),
-            nn.Linear(65, num_classes)
+            nn.Dropout(0.3),
+            
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            
+            nn.Linear(64, num_classes)
         )
 
     def forward(self, x):
